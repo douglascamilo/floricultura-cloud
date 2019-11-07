a633abd9901e74cfaefe480bdbac4dad60230186
Douglas Sousa <douglascamilo@gmail.com>
2019-11-07 00:48:56 -0300

    [Aula 05] - Configurando o Papertrail

--- loja/src/main/kotlin/br/com/alura/microservice/loja/controller/CompraController.kt
+++ loja/src/main/kotlin/br/com/alura/microservice/loja/controller/CompraController.kt
@@ -1,21 +1,27 @@
 package br.com.alura.microservice.loja.controller
 
 import br.com.alura.microservice.loja.controller.dto.CompraDTO
-import br.com.alura.microservice.loja.model.Compra
+import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO
 import br.com.alura.microservice.loja.service.CompraService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import br.com.alura.microservice.loja.model.Compra
+import org.slf4j.LoggerFactory
 
 @RestController
 @RequestMapping("/compra")
 class CompraController {
+	private val LOG = LoggerFactory.getLogger(CompraController::class.java)
+
 	@Autowired lateinit var compraService: CompraService
 
 	@PostMapping
 	fun realizaCompra(@RequestBody compra: CompraDTO): Compra {
+		LOG.info("Realizando compra: ${compra}")
 		return compraService.realizaCompra(compra)
 	}
 }
--- loja/src/main/kotlin/br/com/alura/microservice/loja/service/CompraService.kt
+++ loja/src/main/kotlin/br/com/alura/microservice/loja/service/CompraService.kt
@@ -9,13 +9,21 @@ import org.springframework.http.ResponseEntity
 import org.springframework.beans.factory.annotation.Autowired
 import br.com.alura.microservice.loja.client.FornecedorClient
 import br.com.alura.microservice.loja.model.Compra
+import org.slf4j.LoggerFactory
 
 @Service
 class CompraService {
+	private val LOG = LoggerFactory.getLogger(CompraService::class.java)
+
 	@Autowired private lateinit var fornecedorClient: FornecedorClient
 
 	fun realizaCompra(compra: CompraDTO): Compra {
-		val info = fornecedorClient.getInfoPor(compra.endereco.estado)
+		val estado = compra.endereco.estado
+		
+		LOG.info("Buscando informacoes de fornecedor de ${estado}")
+		val info = fornecedorClient.getInfoPor(estado)
+
+		LOG.info("Realizando um pedido...")
 		val pedido = fornecedorClient.realizaPedido(compra.itens)
 
 		return Compra(pedido.id, pedido.tempoDePreparo, compra.endereco.toString());
